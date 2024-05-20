aac14246f332b9a96e4b5cfa4ff0a91e46b75642
Eduardo Rezaghi <eduardo.rezaghi@gmail.com>
2024-05-20 08:31:50 -0300

    Add C++20 for Programmers book examples

---
+++ cpp20forprogrammers/chapters/ch1/fig02_e01.cpp
@@ -0,0 +1,11 @@
+// Program: Welcome to C++ Programming!
+
+#include <iostream> // preprocessor directive to include input/output stream
+
+using std::cout;
+
+// Function main begins program execution
+int main() {
+  cout << "Welcome to C++ Programming!" << std::endl; // Display message
+  return 0; // Indicate that program ended successfully
+}
